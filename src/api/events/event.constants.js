73c0d4b2d4f3fac831bd1d88456aad69cf5975d8
Nandun Bandara <ntbandara3@hotmail.com>
2021-02-03 22:45:40 +0530

    feat(events): add event model and service

---
+++ src/api/events/event.constants.js
@@ -0,0 +1,14 @@
+/**
+ * The status an event can be in
+ * @type {{CANCELLED: string, CLOSED: string, POSTPONED: string, OPEN: string}}
+ */
+const EVENT_STATUS = {
+    OPEN: 'open',
+    CLOSED: 'closed',
+    CANCELLED: 'cancelled',
+    POSTPONED: 'postponed',
+};
+
+export default {
+    EVENT_STATUS
+};
---
+++ src/api/events/event.service.js
@@ -0,0 +1,34 @@
+import Event from './event.model';
+
+/**
+ * Create event in db
+ * @param name
+ * @param description
+ * @param headerImage
+ * @param photos
+ * @param venue
+ * @param fromDate
+ * @param toDate
+ * @param status
+ * @param speakers
+ * @param createdBy
+ * @param host
+ * @returns {Promise<Document<any>>}
+ */
+const createEvent = ({
+                         name, description, headerImage,
+                         photos, venue, fromDate, toDate,
+                         status, speakers, createdBy, host
+}) => {
+    const event = new Event({
+        name, description, headerImage,
+        photos, venue, fromDate, toDate,
+        status, speakers, createdBy, host
+    });
+
+    return event.save();
+};
+
+export default {
+    createEvent,
+};
