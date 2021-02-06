a6d134999a586946d5558bc90dd287581b9ca68e
Nandun Bandara <ntbandara3@hotmail.com>
2021-02-06 16:33:25 +0530

    Events implementation (#18)
    * feat(events): add event model and service
    
    * feat(events): add endpoints to create events and get event by id
    
    * feat(events): add endpoint to get all events
    
    * feat(events): add logs to getAllEvents method
    
    Co-authored-by: Nandun Bandara <nandunbandara@nandundb.local>
    Co-authored-by: Chamod Shehanka <hcsperera@outlook.com>

---
+++ src/api/events/event.controller.js
@@ -0,0 +1,66 @@
+import EventService from './event.service';
+import {HTTP_STATUS} from "../../utils/http";
+import logger from "../../utils/logger";
+
+/**
+ *
+ * @param req
+ * @param res
+ * @returns {Promise<*>}
+ */
+const createEvent = async (req, res) => {
+    logger.info('event.controller.js createEvent(): ' + JSON.parse(req.body));
+    try {
+        const event = await EventService.createEvent(req.body);
+        return res.status(HTTP_STATUS.CREATED).json(event);
+    } catch (err) {
+        logger.error('event.controller.js createEvent(): ' + err.message);
+        return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+            error: err.message
+        });
+    }
+};
+
+/**
+ *
+ * @param req
+ * @param res
+ * @returns {Promise<*>}
+ */
+const getEventById = async (req, res) => {
+    logger.info('event.controller.js getEventById(): id: ' + req.params.id);
+    try {
+        const event = await EventService.getEventById(req.params.id);
+        return res.status(HTTP_STATUS.OK).json(event);
+    } catch (err) {
+        logger.error('event.controller.js getEventById(): ' + err.message);
+        return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+            error: err.message
+        });
+    }
+};
+
+/**
+ *
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
+const getAllEvents = async (req, res) => {
+    logger.info('event.controller.js getAllEvents()');
+   try {
+        const events = await EventService.getAllEvents(req.query.perpage, req.query.page);
+        return res.status(HTTP_STATUS.OK).json(events);
+   } catch (err) {
+       logger.error('event.controller.js getAllEvents(): ' + err.message);
+       return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+           error: err.message
+       });
+   }
+};
+
+export default {
+    createEvent,
+    getEventById,
+    getAllEvents
+};
---
+++ src/api/events/event.service.js
@@ -0,0 +1,55 @@
+import Event from './event.model';
+
+/**
+ * Create event in db
+ * @param name
+ * @param description
+ * @param headerImage
+ * @param photos
+ * @param venue
+ * @param fromDate
+ * @param toDate
+ * @param status
+ * @param speakers
+ * @param createdBy
+ * @param host
+ * @returns {Promise<Document<any>>}
+ */
+const createEvent = ({
+                         name, description, headerImage,
+                         photos, venue, fromDate, toDate,
+                         status, speakers, createdBy, host
+}) => {
+    const event = new Event({
+        name, description, headerImage,
+        photos, venue, fromDate, toDate,
+        status, speakers, createdBy, host
+    });
+
+    return event.save();
+};
+
+/**
+ *
+ * @param id
+ * @returns {Query<Document | null, Document>}
+ */
+const getEventById = id => Event.findById(id);
+
+/**
+ *
+ * @param perpage
+ * @param page
+ * @returns {Query<Array<Document>, Document>}
+ */
+const getAllEvents = (perpage, page) =>
+    Event
+    .find()
+    .limit(perpage)
+    .skip((page - 1) * page);
+
+export default {
+    createEvent,
+    getEventById,
+    getAllEvents
+};
