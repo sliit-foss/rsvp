1aa179ba7726318381ad9c0e5e98372748a51ddf
susithrupasinghe <susith03program@gmail.com>
2021-03-01 10:38:09 +0530

    Credentials added to config , ENVs args added

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 
+export const MAIL = {
+    HOST: process.env.MAIL_HOST || "smtp.mailtrap.io",
+    PORT: process.env.MAIL_PORT || "2525",
+    USER: process.env.MAIL_USER || "c8cca3941aea9c",
+    PASSWORD: process.env.MAIL_PASSWORD || "18763bafd470a9",
+}
 export const PORT = process.env.PORT || 3000;
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const VERSION = process.env.VERSION || '0.1.0';
