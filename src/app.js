fc563989cb035390f84a97e462bfaa520203f278
ebonynon <sithumsandeepap@gmail.com>
2021-03-04 18:14:46 +0530

    Set middleware and cros

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+//import cors from 'cors';
 import { default as connect } from './utils/database';
 import router from './api/routes';
 
 const app = express();
+
+app.use(express.json({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//app.use(cors({ origin: true, credentials: true }));
 app.use('/', router);
 
 connect();
