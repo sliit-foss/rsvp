f287abc707fd37a111a6390031da09776fb30d42
Sithum Bopitiya <sithumsandeepap@gmail.com>
2021-03-23 08:13:11 +0530

    Revert "Simple Health Check working  (#35)"
    This reverts commit 20752303f88e7f03e935897ed42237ba0efb672f.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
-import express from "express";
-import bodyParser from "body-parser";
-import { HealthcheckerSimpleCheck } from "nodejs-health-checker/dist/healthchecker/healthchecker";
+import express from 'express';
+import bodyParser from 'body-parser';
 //import cors from 'cors';
-import { default as connect } from "./utils/database";
-import router from "./api/routes";
+import { default as connect } from './utils/database';
+import router from './api/routes';
 
 const app = express();
 
@@ -12,11 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //app.use(cors({ origin: true, credentials: true }));
-app.use("/", router);
-
-app.get("/healthcheck", (_, res) => {
-  res.send(HealthcheckerSimpleCheck());
-});
+app.use('/', router);
 
 connect();
 
