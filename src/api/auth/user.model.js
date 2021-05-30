2361486cde8c69cb7a18d395b3bf5764a5ec1d77
ebonynon <sithumsandeepap@gmail.com>
2021-05-30 08:48:42 +0530

    Rm password

--- src/api/auth/user.model.js
+++ src/api/auth/user.model.js
@@ -9,7 +9,6 @@ const userSchema = new Schema(
     {
         username: {type: String, required: true, unique: true},
         email: {type: String, required: true, unique: true},
-        password: {type: String, required: true}
     }
 );
 
