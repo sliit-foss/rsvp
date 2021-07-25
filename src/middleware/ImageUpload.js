a67bc93f40eda0c74ae127709c3f958096babe10
Akalanka Perera <akalankaperera128@gmail.com>
2021-07-25 17:38:13 +0530

    code cleanup

--- src/middleware/ImageUpload.js
+++ src/middleware/ImageUpload.js
@@ -11,13 +11,8 @@ const ImageUpload = async (base64Img, filePath) => {
   const uuid = v4();
 
   const downloadURL =
-    'https://firebasestorage.googleapis.com/v0/b/' +
-    bucket.name +
-    '/o/' +
-    encodeURIComponent(`images/${filePath}`) +
-    '?alt=media&token=' +
-    uuid;
-
+  `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(`images/${filePath}`)}?alt=media&token=${uuid}`
+  
   await file.save(imageBuffer, {
     metadata: {
       contentType: mimeType,
