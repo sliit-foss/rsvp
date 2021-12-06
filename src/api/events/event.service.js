0e63a8e416c9a86901727548cbdfa07641c483cf
Akalanka Perera <akalankaperera128@gmail.com>
2021-12-06 22:39:47 +0530

    Removed required check of event speakers during update

--- src/api/events/event.service.js
+++ src/api/events/event.service.js
@@ -154,11 +154,6 @@ const updateEventByID = async (id, body, user) => {
     user,
     'You can only make changes to events published by your faculty'
   );
-  if (body.speakers && body.speakers.length == 0) {
-    throw {
-      message: 'There needs to be at least one speaker',
-    };
-  }
   const eventName = body.name
     ? body.name
     : (await Event.findById(id).select(['name'])).name;
@@ -173,7 +168,6 @@ const updateEventByID = async (id, body, user) => {
   if (body.speakers) {
     body.speakers = await uploadSpeakerPhotos(body.speakers, eventName);
   }
-
 
   if (body.joinLink) {
     const attendees = event.attendees;
